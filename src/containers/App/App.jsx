9e0dd97c5cf6d4b1e721d144e2ba59c898f67261
AnastasiaGer <aagermanova@gmail.com>
2021-03-28 20:16:57 -0700

    HomePage

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,9 +1,19 @@
 import styles from './App.module.css';
 import PeoplePage from '@containers/PeoplePage/PeoplePage.jsx';
+import HomePage from '@containers/HomePage/HomePage.jsx';
+import {NavLink, Route, BrowserRouter} from 'react-router-dom'
 
 const App = () => {
   return (
-    <PeoplePage />
+    <>
+    <BrowserRouter> 
+      <NavLink to="/" exact>Home</NavLink>
+      <NavLink to="/people" exact> People</NavLink>
+
+      <Route path="/" exact component={HomePage} />
+      <Route path="/people" exact component={PeoplePage} />
+    </BrowserRouter>
+    </>
   )
 }
 
